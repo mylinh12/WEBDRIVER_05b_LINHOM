fd19cce84a5a75321bb276365657223aa569a3e1
My Linh <shinichi121088@gmail.com>
2020-04-10 04:07:11 +0700

    Excersise Xpath (Part 1)

--- src/selenium_api/Topic_02_Xpath_Css_Locator.java
+++ src/selenium_api/Topic_02_Xpath_Css_Locator.java
@@ -1,10 +1,12 @@
 package selenium_api;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -15,26 +17,98 @@ public class Topic_02_Xpath_Css_Locator {
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
-		driver.get("http://demo.guru99.com/v4/");
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
 
 	@Test
-	public void TC_01_CheckUrlAndTitle() {
-		/*Xpath: */
-		//01
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("mylinh@mailinator.comm");
-		
-		//02
-		driver.findElement(By.xpath("//input[@class='input-text required-entry validate-email validation-failed']")).sendKeys("mylinh@mailinator.com");
-		
-		//03
-		driver.findElement(By.xpath(".//input[@title='Email Address']")).sendKeys("mylinh@mailinator.com");
-		
-		//04
-		driver.findElement(By.xpath("//input[@name='login[username]']")).sendKeys("mylinh@mailinator.com");
-		
+	public void TC_02_LoginWithUserPassEmpty() {
+
+		// Step 01: Go to url: http://live.demoguru99.com/
+		driver.get("http://live.demoguru99.com/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		// Step 02: Click on 'My Account' link to navigate to Login page
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+
+		// Step 03: Click Login button
+		driver.findElement(By.id("send2")).click();
+
+		// Step 04: Verify error message at 2 fields
+		String userameEmptyMessage = driver.findElement(By.id("advice-required-entry-email")).getText();
+		Assert.assertEquals(userameEmptyMessage, "This is a required field.");
+
+		String passwordEmptyMessage = driver.findElement(By.id("advice-required-entry-pass")).getText();
+		Assert.assertEquals(passwordEmptyMessage, "This is a required field.");
+
+	}
+
+	@Test
+	public void TC_03_LoginWithEmailInvalid() {
+
+		// Step 01: Go to url: http://live.demoguru99.com/
+		driver.get("http://live.demoguru99.com/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		// Step 02: Click on 'My Account' link to navigate to Login page
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+
+		// Step 03: input into a invalid email
+		driver.findElement(By.cssSelector("#email")).sendKeys("dsdasdfds@dsfssd");
+
+		// Step 04: Click Login button
+		driver.findElement(By.id("send2")).click();
+
+		// Step 05: Verify error message
+		String passwordEmptyMessage = driver.findElement(By.id("advice-validate-email-email")).getText();
+		Assert.assertEquals(passwordEmptyMessage,
+				"Please enter a valid email address. For example johndoe@domain.com.");
+	}
+
+	@Test
+	public void TC_04_LoginWithPasswordLessThan6Charactors() {
+
+		// Step 01: Go to url: http://live.demoguru99.com/
+		driver.get("http://live.demoguru99.com/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		// Step 02: Click on 'My Account' link to navigate to Login page
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+
+		// Step 03: input into a invalid email
+		driver.findElement(By.cssSelector("#email")).sendKeys("automation@mailinator.com");
+
+		// Step 04: input into a password value
+		driver.findElement(By.id("pass")).sendKeys("123");
+
+		// Step 05: Click Login button
+		driver.findElement(By.id("send2")).click();
+
+		// Step 06: Verify error message
+		String passwordInccorrectMessage = driver.findElement(By.id("advice-validate-password-pass")).getText();
+		Assert.assertEquals(passwordInccorrectMessage,
+				"Please enter 6 or more characters without leading or trailing spaces.");
+
+	}
+
+	@Test
+	public void TC_06_CreateAnAccount() {
+
+		// Step 01: Go to url: http://live.demoguru99.com/
+		driver.get("http://live.demoguru99.com/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		// Step 02: Click on 'My Account' link to navigate to Login page
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+
+		// Step 03: input into a invalid email
+		driver.findElement(By.cssSelector("#email")).sendKeys("automation" + randomEmail() + "@mailinator.com");
+
+	}
+
+	public int randomEmail() {
+		Random rand = new Random();
+		int number = rand.nextInt(999999);
+		return number;
 	}
 
 	@AfterClass
