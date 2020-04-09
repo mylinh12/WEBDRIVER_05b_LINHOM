c8dbc99476a6ff7f03d3c335661c3c24e955be4a
My Linh <shinichi121088@gmail.com>
2020-04-09 18:38:19 +0700

    Delete which lib does not be used

--- src/selenium_api/Topic_02_Xpath_Css_Locator.java
+++ src/selenium_api/Topic_02_Xpath_Css_Locator.java
@@ -1,15 +1,13 @@
 package selenium_api;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Topic_02_Xpath_Css_Locator {
 	WebDriver driver;
